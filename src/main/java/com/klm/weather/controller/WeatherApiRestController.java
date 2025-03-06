8ace3af426abc614d9c7c4e05d7da493d1b1cd95
Yuvaraj <yuvaraj.sf.java@gmail.com>
2025-03-06 13:26:29 +0530

    Added Controller layer to invoke Service operations and enabled endpoints

--- src/main/java/com/klm/weather/controller/WeatherApiRestController.java
+++ src/main/java/com/klm/weather/controller/WeatherApiRestController.java
@@ -1,9 +1,50 @@
 package com.klm.weather.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import com.klm.weather.model.Weather;
+import com.klm.weather.service.WeatherService;
+
 @RestController
+@RequestMapping("/weather")
 public class WeatherApiRestController {
 
+	private final WeatherService service;
+
+    public WeatherApiRestController(WeatherService service) {
+        this.service = service;
+    }
+    
+    @PostMapping
+    public ResponseEntity<Weather> createWeather(@RequestBody Weather weather) {
+        Weather createdWeather = service.saveWeather(weather);
+        return ResponseEntity.status(201).body(createdWeather);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Weather>> getWeather(
+            @RequestParam(required = false) String date,
+            @RequestParam(required = false) String city,
+            @RequestParam(required = false) String sort) {
+        
+        List<String> cities = city != null ? List.of(city.toLowerCase().split(",")) : null;
+        List<Weather> weatherList = service.getAllWeatherByOptionalParams(date, cities, sort);
+
+        return ResponseEntity.ok(weatherList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Weather> getWeatherById(@PathVariable int id) {
+    	Optional<Weather> weather = service.getWeatherById(id);
+    	if (weather.isPresent()) {
+    	    return ResponseEntity.ok(weather.get());
+    	} else {
+    	    return ResponseEntity.notFound().build();
+    	}
+    }
 
 }
